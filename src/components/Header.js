4e14d3429b3759892dc59f92554f81d8ffc77d11
Eloissa <mariaeloissa.andal@tup.edu.ph>
2023-12-27 16:00:15 +0800

    update

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <div className="header">
+            <h1>Rodo List</h1>
+        </div>
+    )
+}
+
+export default Header;
